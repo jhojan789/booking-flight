813d6212d5b0b8a1edc19ff8089f5b0187a7ab14
Jhojan Tobón <jhojantobonm@gmail.com>
2024-01-10 12:57:53 -0500

    Added properties and methods of a machine.

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { useMachine } from "@xstate/react";
 import { bookingMachine } from "../machines/bookMachine";
+import { log } from "xstate/lib/actions";
 
 export function BaseLayout() {
   const [state, send] = useMachine(bookingMachine);
-
-  console.log(state);
+  console.log("Current state:", state.value);
+  console.log("Current context: ", state.context);
+  console.log("Previous event: ", state.event);
+  console.log("Actions: ", state.actions);
+  console.log("Activities: ", state.activities);
+  console.log("History: ", state.history);
+  console.log("Meta: ", state.meta);
+  console.log("NextEvents: ", state.nextEvents);
+  console.log(
+    "State init is equal to the current state? ",
+    state.matches("init")
+  );
+  console.log(
+    "State tickets is equal to the current state? ",
+    state.matches("tickets")
+  );
+  console.log(
+    "Can I perform FINISH event in the current state?",
+    state.can("FINISH")
+  );
+  console.log(
+    "Can I perform START event in the current state?",
+    state.can("START")
+  );
+  console.log("Current machine", state);
   return <div>Hello</div>;
 }
