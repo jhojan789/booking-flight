d46300de33930b35b97f7351abe94ddf63c6632b
Jhojan Tobón <jhojantobonm@gmail.com>
2024-01-12 17:33:49 -0500

    Fixed logic to show state.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Nav({ state, send }) {
   return (
     <nav>
-      <h3>Step {state.value}</h3>
+      <h3>Step {typeof state.value !== "object" ? state.value : "search"}</h3>
       {!state.matches("init") && !state.matches("tickets") && (
         <button onClick={() => send("CANCEL")}>Cancel</button>
       )}
